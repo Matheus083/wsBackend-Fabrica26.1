f509c3ed554881c2e556ad5785a9003a0040f4ec
Matheus083 <matheus2007n@gmail.com>
2026-03-21 10:14:09 -0300

    build(django, structure): I created the project structure. Docker + Bank + Drivers.

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+import pymysql
+
+# Faz o PyMySQL se passar pelo driver oficial.
+pymysql.install_as_MySQLdb()
+
+# PULO DO GATO: Enganar a checagem de versão do Django.
+# "Ei Django, finja que minha versão é a 2.2.1"
+pymysql.version_info = (2, 2, 1, "final", 0)
